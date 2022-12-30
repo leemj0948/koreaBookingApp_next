9bec51d7e204528557244b243d1b59620b53f86e
mj MackBook <leemj0948@gmail.com>
2022-12-30 18:58:12 +0900

    add: calendar 라이브러리 적용, intl api 활용 함으로 moment js 제거

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 import GlobalStyle from '@src/styles/globalStyle';
+import '@src/styles/calendar.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
